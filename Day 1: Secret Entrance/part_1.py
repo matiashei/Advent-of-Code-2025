0fb602a0389c863729bb236474c84d3293f4f310
Matias Heino <matias.heino@hotmail.com>
2025-12-01 12:00:22 +0200

    Day 1, parts 1 and 2

---
+++ Day 1: Secret Entrance/part_1.py
@@ -0,0 +1,25 @@
+dial = 50
+counter = 0
+
+with open("input.txt") as f:
+    for line in f:
+        value = int(line[1:])
+        if line[0] == "L":
+            value = -value
+
+        if value < 0:
+            value = abs(value)
+            for i in range(value):
+                dial -= 1
+                if dial == -1:
+                    dial = 99
+
+        else:
+            for i in range(value):
+                dial += 1
+                if dial == 100:
+                    dial = 0
+
+        if dial == 0:
+                    counter += 1
+print(counter)
