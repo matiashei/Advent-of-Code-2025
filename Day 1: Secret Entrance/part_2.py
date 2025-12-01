27d27938d24e81675318ea8f8b0c99414e2cf942
Matias Heino <matias.heino@hotmail.com>
2025-12-01 17:23:08 +0200

    Refactor Day 1

--- Day 1: Secret Entrance/part_2.py
+++ Day 1: Secret Entrance/part_2.py
@@ -4,7 +4,9 @@ counter = 0
 with open("input.txt") as f:
     for line in f:
         value = int(line[1:])
-        if line[0] == "L":
+        direction = line[0]
+
+        if direction == "L":
             value = -value
 
         if value < 0:
