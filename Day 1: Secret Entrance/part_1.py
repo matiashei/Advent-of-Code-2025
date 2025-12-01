27f5f849586512f5c371b4304c6d397b2c90e1c0
Matias Heino <matias.heino@hotmail.com>
2025-12-01 17:24:41 +0200

    Refactor Day 1 part 1

--- Day 1: Secret Entrance/part_1.py
+++ Day 1: Secret Entrance/part_1.py
@@ -4,9 +4,13 @@ counter = 0
 with open("input.txt") as f:
      for line in f:
         value = int(line[1:])
-        if line[0] == "L":
+        direction = line[0]
+
+        if direction == "L":
           value = -value
+
         dial = (dial + value) % 100
+
         if dial == 0:
           counter += 1
 
